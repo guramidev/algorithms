5c8b9939db9ab57c205fba6026fec5a02c4eff42
Gurami Dagundaridze <g.dagundaridze@adjarabet.com>
2019-07-29 17:27:21 +0400

    tweaked insertion sort implementation

--- sorting/insertion-sort/insertion-sort.cpp
+++ sorting/insertion-sort/insertion-sort.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 void insertionSort(int arr[], int size) {
-  for(int i = 1; i < size; i++) {
-    for(int j = i; j >= 0; j--) {
-      if (arr[j] < arr[j - 1]) {
-        int temp = arr[j];
-        arr[j] = arr[j - 1];
-        arr[j - 1] = temp;
-      } else {
-        break;
-      }
+  int i, j;
+
+  for(i = 1; i < size; i++) {
+
+    int temp = arr[i];
+    for(j = i; j >= 0 && arr[j - 1] > temp; j--) {
+      arr[j] = arr[j - 1];
     }
+    arr[j] = temp;
+
   }
 }
 
